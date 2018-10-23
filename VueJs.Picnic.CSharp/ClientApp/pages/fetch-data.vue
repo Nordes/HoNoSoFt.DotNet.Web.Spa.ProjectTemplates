7d65d8876ef32e215c109ad22fa3746995f3e6f7
Nordes Lamarre <nordes@gmail.com>
2018-10-23 22:17:38 +0900

    #8 - Add the paging (as I did in the past)

--- VueJs.Picnic.CSharp/ClientApp/pages/fetch-data.vue
+++ VueJs.Picnic.CSharp/ClientApp/pages/fetch-data.vue
@@ -2,24 +2,33 @@
   <div>
     <page-title title="Weather Forecast"/>
     <div class="flex one clear-side">
-    <table v-if="forecasts">
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Temp. (C)</th>
-          <th>Temp. (F)</th>
-          <th>Summary</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(forecast, index) in forecasts" :key="index">
-          <td>{{ forecast.dateFormatted }}</td>
-          <td>{{ forecast.temperatureC }}</td>
-          <td>{{ forecast.temperatureF }}</td>
-          <td>{{ forecast.summary }}</td>
-        </tr>
-      </tbody>
-    </table>
+      <table v-if="forecasts">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Temp. (C)</th>
+            <th>Temp. (F)</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(forecast, index) in forecasts" :key="index">
+            <td>{{ forecast.dateFormatted }}</td>
+            <td>{{ forecast.temperatureC }}</td>
+            <td>{{ forecast.temperatureF }}</td>
+            <td>{{ forecast.summary }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <!-- paging -->
+      <div class="flex seven center">
+        <div><span class="button paging" :disabled="previousDisabled" @click="loadPage(currentPage - 1)">Previous</span></div>
+        <div v-for="(n, index) in totalPages" :key="index">
+          <span 
+            :class="'button paging' + (n == currentPage ? ' success active' : '')" 
+            @click="loadPage(n)">{{n}}</span></div>
+        <div><span class="button paging" :disabled="nextDisabled" @click="loadPage(currentPage + 1)">Next</span></div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,22 +37,32 @@
   .clear-side {
     padding: 0 0.5em;
   }
+  .paging {
+    text-align:center;
+    display:block;
+  }
 </style>
 
 <script>
 export default {
+  computed: {
+    previousDisabled: function () {
+      return (this.currentPage == 1 ? true : false);
+    },
+    nextDisabled: function () {
+      return (this.currentPage < this.totalPages ? false : true);
+    },
+    totalPages: function () {
+      return Math.ceil(this.total / this.pageSize)
+    }
+  },
+
   data() {
     return {
-      // forecasts: [{
-      //   dateFormatted: "1223/12/12",
-      //   temperatureC: 23,
-      //   temperatureF: 82,
-      //   summary: 'New-York'
-      // }]
       forecasts: null,
-      total: 0,
-      pageSize: 5,
-      currentPage: 1
+      total: 1,
+      pageSize: 10,
+      currentPage: -1
     }
   },
 
@@ -51,6 +70,12 @@ export default {
     async loadPage (page) {
       // ES2017 async/await syntax via babel-plugin-transform-async-to-generator
       // TypeScript can also transpile async/await down to ES5
+
+      if (this.currentPage === page || page < 1 || page > this.totalPages) {
+        // Nothing to do... we're at some extremity.
+        return;
+      }
+
       this.currentPage = page
       try {
         var from = (page - 1) * (this.pageSize)
