5a84f77664653b0302aa97d6cfb4b8282af597f3
Nordes Lamarre <nordes@gmail.com>
2018-11-07 00:07:26 +0900

    Update home and also fontello's

--- VueJs.Picnic.CSharp/ClientApp/router/routes.js
+++ VueJs.Picnic.CSharp/ClientApp/router/routes.js
@@ -2,10 +2,10 @@ import Home from 'pages/home'
 import CounterExample from 'pages/counter-example'
 import FetchData from 'pages/fetch-data'
 import About from 'pages/about'
-import PageNotFound from 'pages/page-not-found'
+import NotFound404 from 'pages/404'
 
 export const routes = [
-  { showMenu: false, name: '404', path: '/404', component: PageNotFound },
+  { showMenu: false, name: '404', path: '/404', component: NotFound404 },
   { showMenu: true, name: 'home', path: '/', component: Home, display: 'Home', icon: 'icon-home' },
   { showMenu: true, name: 'counter', path: '/counter', component: CounterExample, display: 'Counter', icon: 'icon-counter' },
   { showMenu: true, name: 'fetch-data', path: '/fetch-data', component: FetchData, display: 'Fetch data', icon: 'icon-list-bullet' },
--- VueJs.PicnicTable.CSharp/ClientApp/router/routes.js
+++ VueJs.PicnicTable.CSharp/ClientApp/router/routes.js
@@ -3,10 +3,10 @@ import CounterExample from 'pages/counter-example'
 import FetchData from 'pages/fetch-data'
 import ExtraStuff from 'pages/extra-stuff'
 import About from 'pages/about'
-import PageNotFound from 'pages/page-not-found'
+import NotFound404 from 'pages/404'
 
 export const routes = [
-  { showMenu: false, name: '404', path: '/404', component: PageNotFound },
+  { showMenu: false, name: '404', path: '/404', component: NotFound404 },
   { showMenu: true, name: 'home', path: '/:lang?', component: Home, display: 'Home', i18n: 'route.home', icon: 'icon-home' },
   { showMenu: true, name: 'counter', path: '/:lang?/counter', component: CounterExample, i18n: 'route.counter', icon: 'icon-counter' },
   { showMenu: true, name: 'fetch-data', path: '/:lang?/fetch-data', component: FetchData, i18n: 'route.fetchData', icon: 'icon-list-bullet' },
