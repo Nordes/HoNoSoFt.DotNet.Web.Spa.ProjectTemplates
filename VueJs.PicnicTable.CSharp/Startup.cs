7cad59047cdbd9d24fbb366ec53c00672c3991a0
Nordes Menard-Lamarre <nordes.menard-lamarre@konicaminolta.com>
2019-12-23 17:14:21 +0900

    Update NPM Package and fix startup

--- VueJs.PicnicTable.CSharp/Startup.cs
+++ VueJs.PicnicTable.CSharp/Startup.cs
@@ -24,7 +24,7 @@ namespace VueJs.PicnicTable.CSharp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(mvcOptions => { mvcOptions.EnableEndpointRouting = false; });
             services.AddSwaggerGen(c =>
                 {
                     c.SwaggerDoc("v1", new Info {
@@ -67,9 +67,9 @@ namespace VueJs.PicnicTable.CSharp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.EnvironmentName.Equals("Development"))
             {
                 app.UseDeveloperExceptionPage();
             }
